98b8d6b142b5a00a55835017f90e95c8dc000611
alextanhongpin <alextan220990@gmail.com>
2017-05-26 00:21:47 +0800

    Update nodejs version

--- app/server.js
+++ app/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.status(200).json({
-    ok: true
+    ok: 'car'
   })
 })
 
